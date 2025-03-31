ed14f031b01329aa3931debce669db9ca147590a
makowithyou <mako.withyou@gmail.com>
2025-03-31 13:07:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $("#Slider").slick({
     dots: true,
     autoplay: true,
     centerMode: true,
+    slidesToShow: 1,
+    centerPadding: "18%",
     prevArrow: '<span class="prev-arrow"></span>',
     nextArrow: '<span class="next-arrow"></span>',
     responsive: [
